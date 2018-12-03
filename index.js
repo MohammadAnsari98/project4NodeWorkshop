2777ffcc574fee1c78b7e81c7b9e7b5059625e8e
MohammadAnsari98 <iammohammadansari@gmail.com>
2018-12-03 12:45:16 +0330

    show function polished

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 function addTask(params){
 	const fs = require('fs');
 	console.log('It worked!!');
-	let appendText=params.id+'\n'+params.title+'\n'+params.text+'\n';
+	let appendText='\n'+params.id+'\n'+params.title+'\n'+params.text+'\n';
 fs.appendFileSync('tasks.txt',appendText);
 }
 
 function showTask(){
 	const fs = require('fs');
-	console.log("salamamasm")
-
+	var contents = fs.readFileSync('tasks.txt', 'utf8');
+	console.log(contents);
 
 }
 
